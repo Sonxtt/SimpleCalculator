6c2c0b901b8bb0f10c3f99cc3c64ba4d68a1caa8
Sonya <sonechcka2008@gmail.com>
2024-11-08 19:33:17 +0200

    New brech

--- SimpleCalculator/addition.cpp
+++ SimpleCalculator/addition.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
+using namespace std;
 
 int add(int a, int b) {
     return a + b;
-}
-
-int main() {
-    int num1 = 5, num2 = 10;
-    std::cout << "Suma: " << add(num1, num2) << std::endl;
 }
