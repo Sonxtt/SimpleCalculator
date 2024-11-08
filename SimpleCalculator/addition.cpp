77bdfd64b8b60dd338b11b0646338e14e9f2da0c
Sonya <sonechcka2008@gmail.com>
2024-11-08 19:40:10 +0200

    mnochena

--- SimpleCalculator/addition.cpp
+++ SimpleCalculator/addition.cpp
@@ -1,10 +1,7 @@
+
 #include <iostream>
+using namespace std;
 
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int num1 = 5, num2 = 10;
-    std::cout << "Suma: " << add(num1, num2) << std::endl;
+int multiply(int a, int b) {
+    return a * b;
 }
