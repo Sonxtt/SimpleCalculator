f5425a39cda045bede4ae02a0e24fdb47b5ba838
Sonya <sonechcka2008@gmail.com>
2024-11-08 17:07:46 +0200

    Dodavana

--- SimpleCalculator/addition.cpp
+++ SimpleCalculator/addition.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
+using namespace std;
 
 int add(int a, int b) {
     return a + b;
-}
-
-int main() {
-    int
-    int num1 = 5, num2 = 10;
-    std::cout << "Suma: " << add(num1, num2) << std::endl;
 }
